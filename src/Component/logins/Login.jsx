fb03da10358786f3a6f5a51066820cc3dea043d4
Hoang Vu <tranhoangvu2205@gmail.com>
2024-04-04 20:49:44 +0700

    test

--- src/Component/logins/Login.jsx
+++ src/Component/logins/Login.jsx
@@ -2,23 +2,50 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import styles from "./style.module.css";
 import { UserOutlined } from "@ant-design/icons";
-import {  useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getLoginInfo } from "../../Redux/thunk/userThunk";
 import Loading from "../../Common/loading/Loading";
+import { loginApi } from "../../Services/userService";
+
 const Login = () => {
-  const {loadingPage} = useSelector(state => state.modalReducer)
+  const { loadingPage } = useSelector((state) => state.modalReducer);
   const [loginInfo, setLoginInfo] = useState({ username: "", password: "" });
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    await dispatch(getLoginInfo(loginInfo,navigate))
+    await dispatch(getLoginInfo(loginInfo, navigate));
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("access_Token");
+    if (token) {
+      navigate('/admin');
+    }
+  }, []);
+
+  const handleLogin = async () => {
+    try {
+      const { username, password } = loginInfo;
+      const response = await loginApi(username, password);
+      console.log(response);
+      if(response && response.data.access_Token){
+        localStorage.setItem("access_Token", response.data.access_Token)
+        navigate("/admin");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+    
+    
+   
+    
   };
 
   const handleChange = (e) => {
     const { value, name } = e.target;
     setLoginInfo({ ...loginInfo, [name]: value });
   };
+
   return (
     <>
       <div className={styles.background_container}>
@@ -28,10 +55,7 @@ const Login = () => {
               <UserOutlined className="bg-blue-400 text-white text-4xl rounded-full p-5 leading-5 " />
               <h2>Sign in</h2>
             </div>
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col gap-3  w-80 "
-            >
+            <form onSubmit={handleSubmit} className="flex flex-col gap-3  w-80 ">
               <input
                 type="text"
                 placeholder="Username"
@@ -40,13 +64,14 @@ const Login = () => {
               />
               <input
                 type="password"
-                placeholder="password"
+                placeholder="Password"
                 name="password"
                 onChange={handleChange}
               />
               <button
                 type="submit"
                 className="bg-blue-400 hover:bg-blue-500 duration-300 cursor-pointer "
+                onClick={() => handleLogin()}
               >
                 Login
               </button>
@@ -55,8 +80,7 @@ const Login = () => {
           </div>
         </div>
       </div>
-      {loadingPage ? <Loading/> :''}
-      
+      {loadingPage ? <Loading /> : ""}
     </>
   );
 };
